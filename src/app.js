9737dbff24a3e2c2fc59bdaf502f6a7d54b40ea9
Danilo Leão <olin_ad@hotmail.com>
2023-03-07 10:49:43 -0300

    New branch: signup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import { SignUp } from "./pages/Signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/home" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </Linkr>
